148ed6f07ea01ab03e3010788c80042fc9653ccc
7kia <il7kia@yandex.ru>
2017-10-08 14:30:33 +0400

    Добавлена страница входа(есть только интерфейс) и админа(пустая)

---
+++ WebProject/src/AppBundle/Controller/AdminController.php
@@ -0,0 +1,22 @@
+<?php
+
+// src/AppBundle/Controller/SecurityController.php
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class AdminController extends Controller
+{
+     /**
+     * @Route("/admin")
+     */
+    public function adminAction()
+    {
+        return $this->render('admin.html.twig', array(
+            //'number' => $number,
+        ));
+    }
+
+}
