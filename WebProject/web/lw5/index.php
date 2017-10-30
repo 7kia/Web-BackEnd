b8371430b834a0414b1148b2374dafe901098c69
7kia <il7kia@yandex.ru>
2017-10-30 11:24:43 +0400

    Задание 3-4 в 5 лабе сделаны, только книги размещаются в не динамической таблице

--- WebProject/web/lw5/index.php
+++ WebProject/web/lw5/index.php
@@ -1,7 +1,23 @@
 <?php
 require_once 'f:\Study\7_Semester\Web-BackEnd\WebProject\vendor\autoload.php';
+require_once 'src\Book.php';
+
+use Entity\Book;
 
 $loader = new Twig_Loader_Filesystem('f:\Study\7_Semester\Web-BackEnd\WebProject\app\Resources\views\htmlTemplates');
 
 $twig = new Twig_Environment($loader, array('debug' => true));
-echo $twig->render('template5Lab.html.twig');
+
+$book1 = new Book();
+
+$bookList = array(
+	Book::generateWithData("First", 2017, 100, 4, "lw5\\img\\1.png"),
+	Book::generateWithData("Second", 2000, 1000, 5, "lw5\\img\\2.png")
+);
+
+echo $twig->render(
+	'template5Lab.html.twig',
+	array(
+		'bookList' => $bookList
+	)
+);
